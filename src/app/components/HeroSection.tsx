2242519d38c9c3784a5956c73a9d039bddcc91af
Nguyễn Trường <cutruong24701@gmail.com>
2024-03-21 00:40:37 +0700

    fix ESlint

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Image from "next/image";
-import avt from "../../../public/image.jpg";
 import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
@@ -10,7 +9,7 @@ const HeroSection = () => {
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-              Hello, I'm{" "}
+              Hello, I{"'m"}
             </span>
             <br />
             <TypeAnimation
@@ -32,14 +31,14 @@ const HeroSection = () => {
             </button>
             <button className="px-1 py-1 rounded-full w-full sm:w-fit mr-4 bg-gradient-to-br from-blue-500 to-pink-500 text-white mt-3">
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-              Dowload CV
+                Dowload CV
               </span>
             </button>
           </div>
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-10">
           <Image
-            src={avt}
+            src={require("../../../public/image.jpg")}
             alt="hero image"
             width={400}
             height={400}
